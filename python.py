f598bd16602e25b07548c53c3d229638912fc18b
Your Name <you@example.com>
2022-05-14 19:00:04 +0200

    this is my commit

---
+++ python.py
@@ -0,0 +1,7 @@
+
+print("hello world")
+print("mohammed albugaili")
+print("01.01.1983")
+print("molecular medicine")
+print("the weather today very bad")
+print("Bioinformatics and biostatistics2022")

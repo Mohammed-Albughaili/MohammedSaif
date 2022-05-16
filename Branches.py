77a04f1a317548e2b0b619bfc1df0cd6e066c7a1
Your Name <you@example.com>
2022-05-16 22:45:56 +0200

    good exersice

---
+++ Branches.py
@@ -0,0 +1,10 @@
+no_1 = float(input("Hey, give me two numbers:"))
+no_2 = float(input())
+if (no_1 + no_2) > 100:
+    print("that is a big number.")
+elif (no_1 + no_2) > 10:
+    print("that is a medicator number.")
+elif (no_1 + no_2) > 5:
+    print("that is a small number.")
+else:
+    print("this is a very small number.")

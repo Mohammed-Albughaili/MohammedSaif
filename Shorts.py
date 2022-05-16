242612c0839830b83ca34396761e4c9a55143e25
Your Name <you@example.com>
2022-05-16 22:57:26 +0200

    for YIF

---
+++ Shorts.py
@@ -0,0 +1,24 @@
+var1 = 20
+var2 = 10
+var3 = 5
+var4 = 2
+
+
+print(var1)
+var1 += 1
+print(var1)
+var1 -= 3
+print(var1)
+var1 *= 4
+print(var1)
+var1 += var2
+print(var1)
+var1 /= 3
+print(var3)
+var3 += 2
+print(var3)
+var3 += var4
+print(var4)
+var4 *= 4
+print(var4)
+var4 -=1

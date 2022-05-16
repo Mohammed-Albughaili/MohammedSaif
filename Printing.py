ea9bf3ead478ca6407197ca9d4d65aa9164d9cee
Your Name <you@example.com>
2022-05-16 22:41:49 +0200

    this is my message

---
+++ Printing.py
@@ -0,0 +1,12 @@
+var1 = "Hello"
+var2 = "Wold"
+var3 = "Mohammed Albugaili"
+age = 28
+status = "Married"
+work_experience= "Resaercher"
+print(var3)
+print(age)
+print("status")
+print("work_experience")
+print("My name is",var3,"I'm",age)
+print(f"learning programming start with {var1} {work_experience}")
